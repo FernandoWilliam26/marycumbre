56c5effb92d47c0259be821f98d8221d58827716
FernandoWilliam26 <fernando.calvo@alumnos.uneatlantico.es>
2025-05-19 14:22:14 +0200

    ultimate y page

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ const deportes = [
     id: "ultimate",
     nombre: "Ultimate",
     descripcion: "Deporte de equipo jugado con un disco volador",
-    imagen: "/placeholder.svg?height=200&width=300",
+    imagen: "/imagenes/ultimate/Fotos/JPG/IMG_6823.jpg?height=200&width=300",
   },
   {
     id: "palas",
